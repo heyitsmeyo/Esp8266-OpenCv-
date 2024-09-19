594293c68a8d14996727c094a9d9dca1ad45b8d2
Tarek Benbrahim <140254531+heyitsmeyo@users.noreply.github.com>
2024-09-19 17:07:12 +0100

    Add files via upload

---
+++ Blink12e12e.ino
@@ -0,0 +1,60 @@
+/*
+  ESP8266 Blink by Simon Peter
+  Blink the blue LED on the ESP-01 module
+  This example code is in the public domain
+
+  The blue LED on the ESP-01 module is connected to GPIO1
+  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+
+  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+
+#include<ESP8266WiFi.h> 
+
+char* ssid = "TP-LINK_4D691A";
+char* password = "02538826800";
+
+
+WiFiServer server(3030);
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+
+
+WiFi.begin(ssid , password);
+
+while (WiFi.status() != WL_CONNECTED){
+  delay(1000);
+  Serial.print("connecting to WIFI ....");
+
+  
+}
+  Serial.print("connected " );
+  Serial.print("ip is "  ); 
+  Serial.print(WiFi.localIP()) ;
+  server.begin();
+  Serial.print("\n Server started");
+}
+// the loop function runs over and over again forever
+void loop() {
+
+  WiFiClient  client = server.available() ;
+  if (client){
+    while(client.connected()){
+      if(client.available()){
+        char received = client.read();
+        if(received == '1') {
+           digitalWrite(LED_BUILTIN, HIGH);
+        }
+        else if (received == '0') {
+          digitalWrite(LED_BUILTIN, LOW) ;
+        }
+      }
+    }
+    client.stop();
+  }
+                  // Wait for two seconds (to demonstrate the active low LED)
+}
